b8e088b48e91317a6c55bf5aabe7920bdb9115b9
VishakaVarmaVimal <varmavimal.v@husky.neu.edu>
2020-02-19 02:57:56 -0500

    circleCI Trigger

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt-get install -y nodejs
+npm install -g npm
+sudo apt install -y mysql-server
